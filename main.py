1db6855627eb04069c8942657a3c482bf4ae3bb6
julus54321 <julus.contact@gmail.com>
2025-03-23 11:41:11 +0000

    changes

--- main.py
+++ main.py
@@ -1,26 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, url_for, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
-def hello_world():
-    return render_template('login.html')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
 
-@app.route("/login", methods=['POST','GET'])
-def login():
-    error = None
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+@app.route("/", methods=['GET'])
+def index():
+    if session.get('logged_in'):
+        return render_template('home.html')
+    else:
+        return render_template('login.html', message="")
+
+@app.route("/register", methods=['POST','GET'])
+def register():
     if request.method == 'POST':
-        if checkpass(request.form['email'],request.form['password']):
-            return loguserin(request.form['email'])
-        else:
-            error = 'invalida'
-    return render_template('login.html', error=error)
+        try:
+            db.session.add(User(username=request.form['username'], password=request.form['password']))
+            db.session.commit()
+            return redirect(url_for('login'))
+        except:
+            return render_template('index.html', message="User already exists")
+    else:
+        return render_template('register.html')
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        u = request.form['username']
+        p = request.form['password']
+        data = User.query.filter_by(username=u, password=p).first()
+        if data is not None:
+            session['logged_in'] = True
+            return redirect(url_for('index'))
+        return render_template('login.html', message="Incorrect Details")
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('index'))
     
-def checkpass(email,password):
-    return True
 
-def loguserin(email):
-    return render_template('home.html')
-
-app.run(host='0.0.0.0',port='80' ,debug=True)
+if(__name__ == '__main__'):
+    with app.app_context():
+        db.create_all()
+    app.secret_key = "PepoleAreNotBlack"
+    app.run(host='0.0.0.0', port='5000',debug=True)
